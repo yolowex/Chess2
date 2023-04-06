6b13f4c17eb185e954f64f8566c635a67c6f3179
Moein <pistolparody@gmail.com>
2023-04-06 20:08:54 +0430

    step 6, create sprite class

---
+++ core/sprite.py
@@ -0,0 +1,20 @@
+import pygame as pg
+
+class Sprite:
+
+    def __init__(self,path:str):
+        self.raw_surface  = pg.image.load(path)
+        self.transformed_surface = self.raw_surface.copy()
+
+    def transform_by_height( self,new_h ):
+        current_h = self.raw_surface.get_height()
+        new_w = self.raw_surface.get_width() * (new_h / current_h)
+
+        self.transformed_surface = pg.transform.scale(self.raw_surface,(new_w,new_h))
+
+    def transform_by_rel( self,rel_x,rel_y ):
+        new_w,new_h = self.raw_surface.get_size()
+        new_w *= rel_x
+        new_h *= rel_y
+
+        self.transformed_surface = pg.transform.scale(self.raw_surface,(new_w,new_h))
