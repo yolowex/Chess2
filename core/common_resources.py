55701552062c994149b857a3eb50788ef2cb512d
Moein <pistolparody@gmail.com>
2023-04-06 19:39:31 +0430

    step 4, create common_resources class

---
+++ core/common_resources.py
@@ -0,0 +1,5 @@
+import pygame as pg
+from core.event_holder import EventHolder
+
+event_holder:EventHolder
+screen:pg.Surface
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pygame as pg
 from pygame.locals import *
 
 from core.event_holder import EventHolder
+from core import common_resources as cr
 
 pg.init()
 
-screen = pg.display.set_mode([800,640])
-event_holder = EventHolder()
+cr.screen = pg.display.set_mode([800,640])
+cr.event_holder = EventHolder()
 
-while not event_holder.should_quit:
-    event_holder.get_events()
+
+
+while not cr.event_holder.should_quit:
+    cr.event_holder.get_events()
