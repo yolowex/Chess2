4200a041852c1d1e146c0f25416f8ab6c38b8703
Moein <pistolparody@gmail.com>
2023-04-06 21:06:33 +0430

    step 9, add tile collision

--- core/game.py
+++ core/game.py
@@ -10,9 +10,8 @@ class Game:
 
     def __init__(self):
         self.board = chess.Board()
-        self.board_map = {}
+        self.board_map = {} # A map that contains every coord and their co-responding rectangle
         self.pieces_map = {}
-        self.board_rect = FRect(cr.screen.get_rect())
 
         self.board_surface:Surface
         self.pieces_surface:Surface
@@ -20,7 +19,7 @@ class Game:
         self.board_rect = FRect(cr.boards_json_dict['classic_board']['board_rect'])
         self.board_sprite = cr.boards_sprite_dict['classic_board']
         self.resize_board()
-
+        self.create_board_tiles()
 
     def resize_board( self ):
         self.board_sprite.transform(cr.screen.get_width(), cr.screen.get_height())
@@ -33,9 +32,33 @@ class Game:
         self.board_rect.w += 1
         self.board_rect.h += 1
 
+    def create_board_tiles( self ):
+        w = self.board_rect.w / 8
+        h = self.board_rect.h / 8
+
+        letters = 'abcdefgh'
+        digits = '12345678'
+
+        for x,letter in zip(range(8),letters):
+            for y,digit in zip(range(8),digits[::-1]):
+                rect = FRect(x*w,y*h,w,h)
+                rect.x += self.board_rect.x
+                rect.y += self.board_rect.y
+                uci = letter+digit
+                self.board_map[uci] = rect
+
+
+
+
+
     def check_events( self ):
-        ...
+        if cr.event_holder.mouse_pressed_keys[0]:
+            for uci in self.board_map:
+                rect = self.board_map[uci]
+                if cr.event_holder.mouse_rect.colliderect(rect):
+                    print(uci)
+
 
     def render( self ):
         cr.screen.blit(self.board_sprite.transformed_surface,[0,0])
-        pg.draw.rect(cr.screen,[0,0,0],self.board_rect,width=20)
+        # pg.draw.rect(cr.screen,[0,0,0],self.board_rect,width=20)
